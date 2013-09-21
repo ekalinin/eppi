cfdf1d52985852bb9c71129baccb061a893b8b82
Eugene Kalinin <e.v.kalinin@gmail.com>
2013-09-22 00:51:03 +0400

    get_files renamed to get_versions; get_packages switched off for a while

--- src/pkg/eppi_pkg_srv_stat.erl
+++ src/pkg/eppi_pkg_srv_stat.erl
@@ -7,8 +7,8 @@
          start_link/0,
          get_packages/0,
          get_packages/1,
-         get_files/0,
-         get_files/1
+         get_versions/0,
+         get_versions/1
         ]).
 
 %% gen_server callbacks
@@ -18,10 +18,8 @@
 -define(SERVER, ?MODULE).
 
 -record(state, {
-        % Package list
-        packages = [],
-        % Files lisy
-        files = []
+        % Versions list
+        versions = []
     }).
 
 %%%===================================================================
@@ -36,10 +34,10 @@ get_packages() ->
 get_packages(Node) ->
     gen_server:call({?SERVER, Node}, {get_packages}).
 
-get_files() ->
-    get_files(node()).
-get_files(Node) ->
-    gen_server:call({?SERVER, Node}, {get_files}).
+get_versions() ->
+    get_versions(node()).
+get_versions(Node) ->
+    gen_server:call({?SERVER, Node}, {get_versions}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -53,34 +51,30 @@ init([]) ->
     {ok, #state{}}.
 
 handle_call({get_packages}, _From, State) ->
-    {reply, State#state.packages, State};
+    {reply, [], State};
 
-handle_call({get_files}, _From, State) ->
-    {reply, State#state.files, State};
+handle_call({get_versions}, _From, State) ->
+    {reply, State#state.versions, State};
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
 handle_cast({start_server, PackagesDir}, State) ->
+    lager:info(" * Starting server from directory: ~s", [PackagesDir]),
     case filelib:is_dir(PackagesDir) of
         true ->
-            % Get packages — each directory name
-            % TODO: remove base directory from path
-            Packages = get_all_directories(PackagesDir),
-            % Get files
-            % TODO: walk subdirectories
-            Files = get_all_files(PackagesDir),
-            % return stats 
-            {noreply, State#state{packages = Packages, files = Files}};
+            Versions = get_local_versions(PackagesDir),
+            lager:info(" * Found versions: ~p", [length(Versions)]),
+            % return
+            {noreply, State#state{versions = Versions}};
         false ->
-            % some log
-            lager:info("Directory doesn't exists: ~s", [PackagesDir]),
+            lager:info(" * Directory doesn't exists."),
             ok = filelib:ensure_dir(PackagesDir),
             ok = file:make_dir(PackagesDir),
-            lager:info("Directory created: ~s", [PackagesDir]),
-            % return empty plugins list
-            {noreply, State#state{packages = [], files = []}}
+            lager:info(" * Directory created."),
+            % return
+            {noreply, State#state{versions = []}}
     end;
 
 
@@ -99,12 +93,30 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-get_all_files(Dir) ->
-    FindFiles = fun(F, Acc) -> [F | Acc] end,
-    filelib:fold_files(Dir, ".*", false, FindFiles, []).
+%%%
+%%% Package — sub-directory
+%%% Version — file in sub-directory
+%%%
 
-get_all_directories(Path) ->
-    lists:filter(
-        fun(X) -> filelib:is_dir(X) end,
-        filelib:wildcard(Path ++ "/*")
-    ).
+%% @doc Internal. For Accumulation files.
+find_files_int(F, Acc) -> [F | Acc].
+
+%% @doc Returns all files in sub-directories
+get_local_versions(Dir) ->
+    Versions = filelib:fold_files(Dir, ".*", true,
+        fun find_files_int/2, []),
+    lists:map(fun filename:basename/1, Versions).
+
+%% @doc Returnds all files in the package's directory
+get_local_versions(Dir, Package) ->
+    Versions = filelib:fold_files(filename:join(Dir, Package),
+        ".*", true, fun find_files_int/2, []),
+    lists:map(fun filename:basename/1, Versions).
+
+%% @doc Returns all sub-directories
+%get_local_packages(Path) ->
+%    Packages = lists:filter(
+%        fun(X) -> filelib:is_dir(X) end,
+%        filelib:wildcard(Path ++ "/*")
+%    ),
+%    lists:map(fun filename:basename/1, Packages).
