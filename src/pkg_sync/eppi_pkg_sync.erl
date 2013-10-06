48a56d2731c46b9aaae2202ec6f54cadca78b352
Eugene Kalinin <e.v.kalinin@gmail.com>
2013-10-06 20:32:08 +0400

    added some comments

--- src/pkg_sync/eppi_pkg_sync.erl
+++ src/pkg_sync/eppi_pkg_sync.erl
@@ -24,9 +24,11 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+%% @doc Get file from pypi
 get_from_pypi(FileName) ->
     eppi_pkg_sync_sup:worker({get_from_pypi, FileName}).
 
+%% @doc Get file from eppi's Node
 get_from_node(FileName, Node) ->
     gen_server:cast({?SERVER, Node}, {send_me_file, FileName, node()}).
 
@@ -41,11 +43,13 @@ handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+%% @doc Some Node asks current Node to send it a file
 handle_cast({send_me_file, FileName, Node}, State) ->
     lager:info("-> Got `send_me_file` for: ~p from: ~p",[FileName, Node]),
     eppi_pkg_sync_sup:worker({send_to_node, FileName, {?SERVER, Node}}),
     {noreply, State};
 
+%% @doc Some Node send a file to a current node
 handle_cast({save_file, FileName, Content}, State) ->
     lager:info("-> Got `save_file` for: ~p",[FileName]),
     eppi_pkg_sync_sup:worker({save_file, FileName, Content}),
